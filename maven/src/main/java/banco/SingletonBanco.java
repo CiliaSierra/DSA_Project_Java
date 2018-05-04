e467cf7832a2ff40a125b092590cd7a240688fcb
steph <stephypadilla08@gmail.com>
2018-05-04 21:24:35 +0200

    Singleton Mundo y Banco

--- maven/src/main/java/banco/SingletonBanco.java
+++ maven/src/main/java/banco/SingletonBanco.java
@@ -1,12 +1,18 @@
 package banco;
 
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
 public class SingletonBanco {
-    private static SingletonBanco instance;
-    private Singleton() {
+
+    private static SingletonBanco instance; //Declaro la instancia
+
+    private Singleton() {//Constructor del singleton DEBE SER PRIVADO
         // Exists only to defeat instantiation.
     }
-    public static SingletonBanco getInstance() {
+
+    public static SingletonBanco getInstance() {//Singleton Pattern
         if(instance == null) {
             instance = new SingletonBanco();
         }
